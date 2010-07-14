f187d731687fbf2a18e975d47be1c086d06868b5
Sir.DG.MV <Sir.DG.MV@89a60058-1626-11df-9b56-19491d043588>
2010-07-14 16:24:57 +0000

    Completada la Clase Event y creado un mini Test

--- trunk/DesarrolloIndependiente/src/signals/Event.java
+++ trunk/DesarrolloIndependiente/src/signals/Event.java
@@ -9,39 +9,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Class Event as a Immutable object
+ * Represents an event.
+ * As an immutable class can not be modified and is thread-safe
+ * location is a long that represents the time of the event
+ * type is a String that represent the type of the event
+ * attributes is a Map that collect de pair of the Object that represent de attributes of the event
  *
+ * Me quedan dudas a la hora de devolver los atributos yo creo que de esta forma es correcto pero tendría que comprobarlo
  * @author USUARIO
  */
-public class Event {
-    private long location;
-    private String type;
-    private Map<Object,Object> attributes;
-
-    public Event(int moment, String type) {
-        this.location = moment;
-        this.type = type;
-         attributes = new HashMap<Object,Object>();
-    }
-
-    public Event(String type) {
-        this.type = type;
-        this.location=System.currentTimeMillis();
-        attributes = new HashMap<Object,Object>();
-    }
+public final class Event {
+    private final long location;
+    private final String type;
+    private final Map<Object,Object> attributes;
 
     public Event(long moment, String type, Map<Object, Object> attributes) {
         this.location = moment;
-        this.type = type;
+        this.type = type.trim();
         this.attributes = attributes;
     }
-    public Event(String type, Map<Object, Object> attributes) {
-        this.location = System.currentTimeMillis();
-        this.type = type;
-        this.attributes = attributes;
-    }
-    //Aqui estamos permitiendo escapar a la referencia. En un principio no debería ser asi
     public Map<Object, Object> getAttributes() {
-        return attributes;
+        return new HashMap<Object,Object>(this.attributes);
     }
 
     public long getMoment() {
--- trunk/DesarrolloIndependiente/src/signals/NoTemporalSeries.java
+++ trunk/DesarrolloIndependiente/src/signals/NoTemporalSeries.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package signals;
 
 import java.util.ArrayList;
@@ -10,10 +9,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * No habría que devolver el evento al pedir uno sino una copia.
+ * Primer evento es el primero en tiempo? o el primero de la lista? igual para el útlimo
  * @author USUARIO
  */
-public class NoTemporalSeries extends Series{
+public class NoTemporalSeries extends Series {
 
     //Parametros serie temporal
     private ArrayList<Event> events;
@@ -21,30 +21,81 @@ public class NoTemporalSeries extends Series{
     private long firstevent;
     private long lastevent;
     private String units;
-    //Añadir mapa para controlar la cnatidad
-    private Map<String,Integer> tpyeevents;
- public NoTemporalSeries(String identifier, String agent,long timeinit, ArrayList<String> imputs, String units) {
-            super(identifier, agent, timeinit);
-            this.imputs = imputs;
-            this.units = units;
-            this.firstevent = -1;
-            this.lastevent = -1;
-            this.tpyeevents=new HashMap<String,Integer>();
-            this.events=new ArrayList<Event>();
+    private Map<String, Long> typeevents;
+
+    public NoTemporalSeries(String identifier, String agent, long timeinit, ArrayList<String> imputs, String units) {
+        super(identifier, agent, timeinit);
+        this.imputs = imputs;
+        this.units = units;
+        this.firstevent = Long.MAX_VALUE;
+        this.lastevent = Long.MIN_VALUE;
+        this.typeevents = new HashMap<String, Long>();
+        this.events = new ArrayList<Event>();
 
 
     }
- public Event getEvent(int index)
- {
-     return this.events.get(index);
- }
- public boolean addEvent(Event even){
-     if(this.events.add(even)){
-         if(this.events.size()==1)
-             this.firstevent=even.getMoment();
-     return true;
-     }
-     else
-         return false;
- }
+
+    public Event getEvent(int index) {
+        Event e=this.events.get(index);
+        return e;
+    }
+
+    public boolean addEvent(Event even) {
+        if (this.events.add(even)) {
+            if (even.getMoment() < this.firstevent) {
+                this.firstevent = even.getMoment();
+            }
+            if (even.getMoment() > this.lastevent) {
+                this.lastevent = even.getMoment();
+            }
+            Long numberObjects;
+            numberObjects = this.typeevents.get(even.getType());
+            if (numberObjects != null) {
+                this.typeevents.put(even.getType(), new Long(numberObjects.longValue() + 1));
+            }
+            else
+            {
+                this.typeevents.put(even.getType(), new Long(1));
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteEvent(int index) {
+        Event e=this.events.get(index);
+        this.events.remove(index);
+         Long numberObjects;
+            numberObjects = this.typeevents.get(e.getType());
+            if (numberObjects != null) {
+                if(numberObjects>1)
+                {
+                this.typeevents.put(e.getType(), new Long(numberObjects.longValue() - 1));
+                }
+                else
+                {
+                    this.typeevents.remove(e.getType());
+                }
+            }
+            if(this.firstevent==e.getMoment())
+            {
+                this.firstevent = Long.MAX_VALUE;
+                for(int i=0;i<this.events.size();i++)
+                {
+                    if(this.firstevent>this.events.get(i).getMoment())
+                        this.firstevent=this.events.get(i).getMoment();
+                }
+            }
+            if(this.lastevent==e.getMoment())
+            {
+                this.lastevent = Long.MIN_VALUE;
+                for(int i=0;i<this.events.size();i++)
+                {
+                    if(this.lastevent<this.events.get(i).getMoment())
+                        this.lastevent=this.events.get(i).getMoment();
+                }
+            }
+        return true;
+    }
 }
