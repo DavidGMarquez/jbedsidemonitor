3a85c2d46584a3e48bcb4276c9a7ff17bbd67ac3
abraham.otero <abraham.otero@89a60058-1626-11df-9b56-19491d043588>
2010-11-09 19:52:59 +0000

    Revisión de código y revison y extension de parte de los test. Se ha creado una nueva clase de utilidad para dar soporte a operaciones con Arrays sin repetir codigo.

--- trunk/DesarrolloIndependiente/test/signals/EventTest.java
+++ trunk/DesarrolloIndependiente/test/signals/EventTest.java
@@ -1,85 +1,74 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package signals;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author USUARIO
- */
 public class EventTest {
 
-    public EventTest() {
+    @Test
+    public void simpleConstructor() {
+        Event event = new Event(new Date().getTime(), "Standard", null);
+        String prueba = event.getType();
+        prueba = new String("Hola");
+        assertEquals(event.getType(), "Standard");
+        assertEquals(new Date().getTime() >= event.getLocation(), true);
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getCopyOfAttributes method, of class Event.
-     */
-   @Test
-    public void testCrear1() {
-        Event e=new Event(new Date().getTime(), "Standard", null);
-        String prueba=e.getType();
-        prueba=new String("Hola");
-        assertEquals(e.getType(),"Standard");
-        assertEquals(new Date().getTime()>=e.getLocation(), true);
-
-        // TODO review the generated test code and remove the default call to fail.
-    }
-   
-   @Test
-       public void testCrear2() {
-            HashMap<String,String> map=new HashMap<String,String>();
-            map.put("Hello", new Integer(1).toString());
-            map.put("Hello2", new Integer(2).toString());
-            map.put("Hello", new Integer(1).toString());
-        Event e=new Event(new Date().getTime(), "Standard", map);
+    @Test
+    public void constructor() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Hello", new Integer(1).toString());
+        map.put("Hello2", new Integer(2).toString());
+        map.put("Hello", new Integer(1).toString());
+        Event e = new Event(new Date().getTime(), "Standard", map);
         assertEquals(e.getCopyOfAttributes().size(), 2);
-        Map<String, String> mapcopy=e.getCopyOfAttributes();
-            mapcopy.put("Hello2", new Integer(20).toString());
-            map.put("Hello2", new Integer(2000).toString());
-        Integer i=new Integer(Integer.parseInt((String)mapcopy.get("Hello2")));
-        System.out.println(i.intValue());
+        Map<String, String> mapcopy = e.getCopyOfAttributes();
+        mapcopy.put("Hello2", new Integer(20).toString());
+        map.put("Hello2", new Integer(2000).toString());
+        Integer i = new Integer(Integer.parseInt((String) mapcopy.get("Hello2")));
         mapcopy.remove("Hello2");
         assertEquals(mapcopy.get("Hello2"), null);
-
-        
-
         assertEquals(mapcopy.size(), 1);
-assertEquals(e.getCopyOfAttributes().size(), 2);
-        Integer i2=new Integer(Integer.parseInt((String)e.getCopyOfAttributes().get("Hello2")));
-        System.out.println(i2.intValue());
+        assertEquals(e.getCopyOfAttributes().size(), 2);
+        Integer i2 = new Integer(Integer.parseInt((String) e.getCopyOfAttributes().get("Hello2")));
         assertEquals(i2.intValue(), 2);
- 
-        // TODO review the generated test code and remove the default call to fail.
     }
 
+
+    @Test
+    public void testEquals() {
+        Event eType1 = new Event(10, "Type1", null);
+        Event eType1Bis = new Event(10, "Type1", null);
+        Event eType11 = new Event(11, "Type1", null);
+        Event eType2 = new Event(10, "Type2", null);
+
+        assertEquals(true, eType1.equals(eType1));
+        assertEquals(true, eType1Bis.equals(eType1));
+        assertEquals(true, eType1.equals(eType1Bis));
+        assertEquals(false, eType11.equals(eType1));
+        assertEquals(false, eType1.equals(eType11));
+        assertEquals(false, eType2.equals(eType1));
+        assertEquals(false, eType1.equals(eType2));
+        assertEquals(false, eType1.equals(new Object()));
+    }
+
+
+    @Test
+    public void testCompare() {
+        Event eType1 = new Event(10, "Type1", null);
+        Event eType1Bis = new Event(10, "Type1", null);
+        Event eType11 = new Event(11, "Type1", null);
+        Event eType2 = new Event(10, "Type2", null);
+
+        assertEquals(true, eType1.compareTo(eType1) == 0);
+        assertEquals(true, eType1Bis.compareTo(eType1) == 0);
+        assertEquals(true, eType1.compareTo(eType1Bis) == 0);
+        assertEquals(true, eType11.compareTo(eType1) > 0);
+        assertEquals(true, eType1.compareTo(eType11) < 0);
+        assertEquals(true, eType2.compareTo(eType1) > 0);
+        assertEquals(true, eType1.compareTo(eType2) < 0);
+    }
 }
--- trunk/DesarrolloIndependiente/test/signals/NewTestSuite.java
+++ trunk/DesarrolloIndependiente/test/signals/NewTestSuite.java
@@ -1,39 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package signals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- *
- * @author USUARIO
- */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({signals.EvenSeriesTest.class,signals.CircularBufferTest.class,signals.TesterWriterService.class,signals.TimeSeriesTest.class,signals.SignalManagerTest.class,signals.TesterReaderService.class,signals.EventTest.class})
+@Suite.SuiteClasses({signals.EvenSeriesTest.class,
+                        signals.CircularBufferTest.class,
+                        signals.TesterWriterService.class,
+                        signals.TimeSeriesTest.class,
+                        signals.SignalManagerTest.class,
+                        signals.TesterReaderService.class,
+                        signals.EventTest.class})
 public class NewTestSuite {
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
 }
