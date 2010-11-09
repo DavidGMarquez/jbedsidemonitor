18c5ab56cb3751646ba7c9e7b8f7cd46d27c90a0
abraham.otero <abraham.otero@89a60058-1626-11df-9b56-19491d043588>
2010-11-09 18:09:21 +0000

    Revisión de código. Correcciones menores y ahora el test no pasa por un error en la clase CircularBuffer.

--- trunk/DesarrolloIndependiente/test/signals/CircularBufferTest.java
+++ trunk/DesarrolloIndependiente/test/signals/CircularBufferTest.java
@@ -1,189 +1,133 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package signals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author USUARIO
- */
 public class CircularBufferTest {
 
     public CircularBufferTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test de Creacion de 2 Buffers con los constructores
-     */
     @Test
-    public void testCrear() {
-        CircularBuffer Test = new CircularBuffer();
-        CircularBuffer Test10 = new CircularBuffer(10);
-
+    public void testConstructor() {
+        CircularBuffer bufferWithAcapacityOff10000 = new CircularBuffer();
+        assertEquals(10000, bufferWithAcapacityOff10000.getCapacity());
+        CircularBuffer bufferWithAcapacityOff10 = new CircularBuffer(10);
+        assertEquals(10, bufferWithAcapacityOff10000.getCapacity());
     }
 
-    /**
-     * Test de escritura de 3 numeros en un array más grande
-     */
     @Test
-    public void testWrite() {
-        float[] datatowrite = new float[3];
-        datatowrite[0] = 1;
-        datatowrite[1] = 2;
-        datatowrite[2] = 3;
-        CircularBuffer instance = new CircularBuffer(5);
-        boolean result = instance.write(datatowrite);
+    public void write3Numbers() {
+        float[] dataToWrite = {1, 2, 3};
+        CircularBuffer buffer = new CircularBuffer(5);
+        boolean result = buffer.write(dataToWrite);
         assertEquals(true, result);
-        assertEquals(true, compareArray(instance.read(0, instance.getSize()), datatowrite,
-                datatowrite.length));
-        assertEquals(0, instance.getIndexold());
-        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(true, compareArray(buffer.read(0, buffer.getSize()), dataToWrite,
+                dataToWrite.length));
+        assertEquals(0, buffer.getIndexold());
     }
 
-    /**
-     * Test de escritura de 5 numeros en un buffer de 5
-     */
     @Test
-    public void testWrite2() {
-        float[] datatowrite = new float[5];
-        datatowrite[0] = 1;
-        datatowrite[1] = 2;
-        datatowrite[2] = 3;
-        datatowrite[3] = 4;
-        datatowrite[4] = 5;
-        CircularBuffer instance = new CircularBuffer(5);
-        boolean result = instance.write(datatowrite);
+    public void write5NumbersAndCheckIndexold() {
+        float[] dataToWrite = {1, 2, 3, 4, 5};
+        CircularBuffer buffer = new CircularBuffer(5);
+        boolean result = buffer.write(dataToWrite);
         assertEquals(true, result);
-        assertEquals(true, compareArray(instance.read(0, instance.getSize()), datatowrite, datatowrite.length));
-        // TODO review the generated test code and remove the default call to fail.
-        assertEquals(0, instance.getIndexold());
+        assertEquals(true, compareArray(buffer.read(0, buffer.getSize()),
+                dataToWrite, dataToWrite.length));
+        assertEquals(0, buffer.getIndexold());
     }
 
-    /** De escritura simple, justo al borde
-     */
     @Test
-    public void testWrite3() {
-        int sizetowrite = 5;
-        float[] datatowrite = this.generateArray(sizetowrite);
-        CircularBuffer instance = new CircularBuffer(5);
-        boolean result = instance.write(datatowrite);
-        assertEquals(true, result);
-        assertEquals(0, instance.getIndexold());
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test de escritura de 6 numeros en un array de 6 dos veces
-     */
-    @Test
-    public void testWrite4() {
+    public void write6RandomNumbersAndCheckIndexold() {
         int sizetowrite = 6;
-        float[] datatowrite = null;
-        datatowrite = this.generateArray(sizetowrite);
-        CircularBuffer instance = new CircularBuffer(6);
-        boolean result = instance.write(datatowrite);
+        float[] dataToWrite = null;
+        dataToWrite = this.generateArray(sizetowrite);
+        CircularBuffer buffer = new CircularBuffer(6);
+        boolean result = buffer.write(dataToWrite);
         assertEquals(true, result);
-        result = instance.write(datatowrite);
+        result = buffer.write(dataToWrite);
         assertEquals(true, result);
-        assertEquals(true, compareArray(instance.read(0, instance.getSize()), datatowrite, sizetowrite));
-        assertEquals(0, instance.getIndexold());
-        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(true, compareArray(buffer.read(0, buffer.getSize()), dataToWrite, sizetowrite));
+        assertEquals(0, buffer.getIndexold());
     }
 
-    /**
-     * Test de escritura de varias escrituras para que se desborde el array
-     */
     @Test
-    public void testWrite5() {
-        int sizetowrite = 4;
-        float[] datatowrite = {1, 2, 3, 4};
-        CircularBuffer instance = new CircularBuffer(5);
-        boolean result = instance.write(datatowrite);
-        assertEquals(0, instance.getIndexold());
-        result = instance.write(datatowrite);
+    public void testWriteSeveralTimesAndCheckData() {
+        int sizeToWrite = 4;
+        float[] dataToWrite = {1, 2, 3, 4};
+        CircularBuffer buffer = new CircularBuffer(5);
+        boolean result = buffer.write(dataToWrite);
+        assertEquals(0, buffer.getIndexold());
+        result = buffer.write(dataToWrite);
 
         assertEquals(true, result);
 
         float[] datacompare = {2, 3, 4, 4, 1};
-        assertEquals(true, compareArray(instance.read(0, instance.getSize()), datacompare, instance.read(0, instance.getSize()).length));
-        assertEquals(3, instance.getIndexold());
+        assertEquals(true, compareArray(buffer.read(0, buffer.getSize()), datacompare, buffer.read(0, buffer.getSize()).length));
+        assertEquals(3, buffer.getIndexold());
         float[] datacompare2 = {3, 4, 4, 1, 2};
-        result = instance.write(datatowrite);
-        assertEquals(true, compareArray(instance.read(0, instance.getSize()), datacompare2, instance.read(0, instance.getSize()).length));
+        result = buffer.write(dataToWrite);
+        assertEquals(true, compareArray(buffer.read(0, buffer.getSize()), datacompare2, buffer.read(0, buffer.getSize()).length));
 
         assertEquals(true, result);
-        assertEquals(2, instance.getIndexold());
-        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(2, buffer.getIndexold());
     }
 
-    /**
-     * Test de lectura simple con un array asignado manualmente
-     */
     @Test
-    public void testRead1() {
-        CircularBuffer instance = new CircularBuffer(5);
+    public void writeManyNumbersAndCheckIndexoldAndLastData() {
+        float[] dataToWrite = this.generateArrayWithConsecutiveIntegers(0, 999);
+        CircularBuffer buffer = new CircularBuffer(1000);
+        boolean result = buffer.write(dataToWrite);
+        assertEquals(true, result);
+        assertEquals(0, buffer.getIndexold());
+        assertEquals(998, buffer.getLastData(), 0.1);
+
+        result = buffer.write(dataToWrite);
+        assertEquals(true, result);
+        assertEquals(998, buffer.getIndexold());
+        assertEquals(998, buffer.getLastData(), 0.1);
+
+        dataToWrite = this.generateArrayWithConsecutiveIntegers(-2, 5);
+        result = buffer.write(dataToWrite);
+        assertEquals(true, result);
+        assertEquals(3, buffer.getIndexold());
+        assertEquals(2, buffer.getLastData(), 0.1);
+    }
+
+    @Test
+    public void simpleRead() {
+        CircularBuffer buffer = new CircularBuffer(5);
         float[] datacompare = {2, 3, 4, 4, 1};
-        instance.write(datacompare);
+        buffer.write(datacompare);
         float[] datacompare2 = {3, 4};
         float[] datacompare3 = {4, 4, 1};
         float[] datacompare4 = {1, 2, 3};
-        assertEquals(true, compareArray(instance.read(1, 2), datacompare2, 2));
-
-        assertEquals(true, compareArray(instance.read(2, 3), datacompare3, 3));
-        assertEquals(true, compareArray(instance.read(4, 3), datacompare4, 3));
-
+        assertEquals(true, compareArray(buffer.read(1, 2), datacompare2, 2));
+        assertEquals(true, compareArray(buffer.read(2, 3), datacompare3, 3));
+        assertEquals(true, compareArray(buffer.read(4, 3), datacompare4, 3));
     }
 
-    /**
-     * Test de lectura y escritura completo
-     */
     @Test
-    public void testReadWrite1() {
-        CircularBuffer instance = new CircularBuffer(9);
+    public void readWhitRandomNumbers() {
+        CircularBuffer buffer = new CircularBuffer(9);
         float[] array1 = generateArray(5);
         float[] array2 = generateArray(5);
         float[] array3 = generateArray(5);
-        instance.write(array1);
-        assertEquals(0, instance.getIndexold());
+        buffer.write(array1);
+        assertEquals(0, buffer.getIndexold());
 
-        assertEquals(true, compareArray(array1, instance.read(0, 5), 5));
-        instance.write(array2);
-        assertEquals(1, instance.getIndexold());
-        assertEquals(true, compareArray(array2, instance.read(5, 5), 5));
-        instance.write(array3);
-        assertEquals(6, instance.getIndexold());
-        assertEquals(true, compareArray(array3, instance.read(1, 5), 5));
+        assertEquals(true, compareArray(array1, buffer.read(0, 5), 5));
+        buffer.write(array2);
+        assertEquals(1, buffer.getIndexold());
+        assertEquals(true, compareArray(array2, buffer.read(5, 5), 5));
+        buffer.write(array3);
+        assertEquals(6, buffer.getIndexold());
+        assertEquals(true, compareArray(array3, buffer.read(1, 5), 5));
     }
 
-    /**
-     * Test de lectura y escritura completo 2
-     */
     @Test
-    public void testReadWrite2() {
+    public void complexReadTest() {
         CircularBuffer instance = new CircularBuffer(9);
         float[] array1 = {11, 12, 13, 14, 15, 16, 17};
         float[] array2 = {21, 22, 23, 24, 25};
@@ -207,14 +151,14 @@ public class CircularBufferTest {
         instance.write(array2);
         assertEquals(4, instance.getIndexold());
 
-        float[] result3 = {22, 23,  24, 25, 17,31, 32, 33, 21};
+        float[] result3 = {22, 23, 24, 25, 17, 31, 32, 33, 21};
         assertEquals(true, compareArray(result3, instance.read(0, 9), 9));
         instance.write(array1);
-        float[] result4 = {16, 17,  24, 25, 11,12, 13, 14, 15};
+        float[] result4 = {16, 17, 24, 25, 11, 12, 13, 14, 15};
         assertEquals(true, compareArray(result4, instance.read(0, 9), 9));
         assertEquals(2, instance.getIndexold());
         instance.write(array2);
-        float[] result5 = {16, 17,  21, 22, 23, 24, 25, 14, 15};
+        float[] result5 = {16, 17, 21, 22, 23, 24, 25, 14, 15};
         assertEquals(true, compareArray(result5, instance.read(0, 9), 9));
         assertEquals(7, instance.getIndexold());
 
@@ -224,29 +168,30 @@ public class CircularBufferTest {
         assertEquals(5, instance.getIndexold());
 
     }
+
     @Test
-    public void testDesbordar() {
+    public void testTooMuchDataToWriteException() {
         CircularBuffer instance = new CircularBuffer(7);
         float[] array1 = this.generateArray(7);
         instance.write(array1);
         CircularBuffer instance2 = new CircularBuffer(6);
-        try{
-        instance2.write(array1);
-        fail("Debería haber saltado la excepcion");}
-        catch(Exception e){
+        try {
+            instance2.write(array1);
+            fail("Debería haber saltado la excepcion");
+        } catch (TooMuchDataToWriteException e) {
             System.out.println(e.getMessage());
         }
-                CircularBuffer instance3 = new CircularBuffer(3);
-        try{
-        instance3.write(array1);
-        fail("Debería haber saltado la excepcion");}
-        catch(Exception e){
+        CircularBuffer instance3 = new CircularBuffer(3);
+        try {
+            instance3.write(array1);
+            fail("Debería haber saltado la excepcion");
+        } catch (TooMuchDataToWriteException e) {
             System.out.println(e.getMessage());
         }
 
     }
 
-    public void mostrarArray(float[] array) {
+    public void showArray(float[] array) {
         System.out.println("-");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
@@ -255,17 +200,12 @@ public class CircularBufferTest {
     }
 
     public boolean compareArray(float[] array1, float[] array2, int tam) {
-        int acum = 0;
         for (int i = 0; i < tam; i++) {
             if (array1[i] != array2[i]) {
-                acum++;
+                return false;
             }
         }
-        if (acum == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return true;
     }
 
     public float[] generateArray(int tam) {
@@ -273,7 +213,14 @@ public class CircularBufferTest {
         for (int i = 0; i < tam; i++) {
             array[i] = (float) Math.random() * 100;
         }
+        return array;
+    }
 
+    public float[] generateArrayWithConsecutiveIntegers(int from, int tam) {
+        float[] array = new float[tam];
+        for (int i = 0; i < tam; i++) {
+            array[i] = i + from;
+        }
         return array;
     }
 }
