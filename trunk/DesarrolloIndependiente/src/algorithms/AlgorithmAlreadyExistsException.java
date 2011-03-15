b84e1362c8b985567dbc9020419b7ec67112ab1b
Sir.DG.MV <Sir.DG.MV@89a60058-1626-11df-9b56-19491d043588>
2011-03-15 09:40:20 +0000

    Añadidas Excepciones y modificados algunos ficheros

---
+++ trunk/DesarrolloIndependiente/src/algorithms/AlgorithmAlreadyExistsException.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import signals.*;
+
+class AlgorithmAlreadyExistsException extends RuntimeException {
+
+    private String message;
+    private Algorithm algorithm;
+
+    public AlgorithmAlreadyExistsException(String message,Algorithm algorithm) {
+        super(message+" Algorithm:"+algorithm.getIdentifier());
+        this.message = message;
+        this.algorithm=algorithm;
+    }
+
+    public Algorithm getAlgorithm() {
+        return algorithm;
+    }
+}
